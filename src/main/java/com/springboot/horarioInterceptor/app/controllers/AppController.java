110a4fd11bddda1aea2f8fde6f3f5216f64eee81
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-02-04 21:39:34 -0500

    primer commit

---
+++ src/main/java/com/springboot/horarioInterceptor/app/controllers/AppController.java
@@ -0,0 +1,16 @@
+package com.springboot.horarioInterceptor.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+	@GetMapping({"/", "/index"})
+	public String index(Model model) {
+		
+		model.addAttribute("titulo", "Bienvenido al horario de atencion de clientes");
+		return "index";
+	}
+}
