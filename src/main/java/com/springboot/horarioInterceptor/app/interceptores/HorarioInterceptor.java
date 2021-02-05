e9362d92802996f9bd4eaa7489f38e6ea60363b0
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-02-04 23:28:53 -0500

    correccion de NullPointer

--- src/main/java/com/springboot/horarioInterceptor/app/interceptores/HorarioInterceptor.java
+++ src/main/java/com/springboot/horarioInterceptor/app/interceptores/HorarioInterceptor.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -46,7 +47,10 @@ public class HorarioInterceptor implements HandlerInterceptor {
 			ModelAndView modelAndView) throws Exception {
 		
 		String mensaje = (String) request.getAttribute("mensaje");
+		
+		if(modelAndView !=null && handler instanceof HandlerMethod) {
 		modelAndView.addObject("horario", mensaje);
+		}
 	}
 
 	
